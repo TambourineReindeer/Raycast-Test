e31e36444cbf483c441df1a9af320b16fec51ec5
Razzy <razzylaqui@gmail.com>
2015-08-10 11:33:53 -0800

    Rename View to Renderer; import opengl3 library

--- renderer.d
+++ renderer.d
@@ -1,3 +1,34 @@
 module renderer;
 
-import
+import std.math;
+import derelict.opengl3;
+
+class Renderer
+{
+    public:
+        Plane proj; // plane upon which view is projected onto screen; "Plane" is a placeholder
+        int SCREEN_X, // screen length in pixels
+            SCREEN_Y; // screen width in pixels
+        float fov; // field of view
+    
+    this()
+    {
+        proj = new Plane(SCREEN_X, SCREEN_Y);
+    }
+    
+    float getFOV()
+    {
+        return fov;
+    }
+    
+    void setFOV(newFOV)
+    {
+        fov = newFOV;
+    }
+    
+    float getDistFromPlane(Plane proj, float refFOV)
+    {
+        int sideA = proj.length / 2;
+        return distance = sideA / tan(refFOV/2);
+    }
+}
